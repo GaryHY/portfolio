103561faacdf585c98c92d50e26dc249b64121e6
GaryHY <henry.gary@hotmail.com>
2024-11-18 02:51:38 +0100

    feat: add layout for main pages of website

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,20 @@
+<script>
+    import Nav from "$lib/sections/Nav.svelte";
+    import Footer from "$lib/sections/Footer.svelte";
+    import "$lib/styles/style.css";
+</script>
+
+<Nav></Nav>
+<div class="space-y">
+    <slot></slot>
+</div>
+<Footer></Footer>
+
+<style>
+    .space-y {
+        --padding-top: 12rem;
+        padding-top: var(--padding-top);
+        padding-bottom: 1rem;
+        min-height: calc(100vh - var(--padding-top));
+    }
+</style>
