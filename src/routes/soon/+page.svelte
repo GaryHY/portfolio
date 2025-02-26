1ff7589e4e22dca0d10dbf686fdadede0c29ec5f
GaryHY <henry.gary@hotmail.com>
2025-02-26 14:41:03 +0100

    feat: add simple hooks.server to redirect to soon page

---
+++ src/routes/soon/+page.svelte
@@ -0,0 +1,23 @@
+<script>
+    import "$lib/styles/style.css";
+</script>
+
+<div class="container center">
+    <p class="text capitalize">coming soon.</p>
+    <p class="name">henga.dev</p>
+</div>
+
+<style>
+    .container {
+        display: grid;
+        place-content: center;
+        height: 100vh;
+    }
+    .text {
+        font-size: 4rem;
+        color: black;
+    }
+    .name {
+        font-size: 2rem;
+    }
+</style>
